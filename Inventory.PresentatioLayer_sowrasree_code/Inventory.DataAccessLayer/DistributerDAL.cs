0b8848dedee2437c0b02ec0780a2261739a0b8c6
sowrasree <54537371+sowrasree@users.noreply.github.com>
2019-09-16 09:40:34 +0530

    Add files via upload

---
+++ Inventory.PresentatioLayer_sowrasree_code/Inventory.DataAccessLayer/DistributerDAL.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using Inventory.Entities;
+using Inventory.Exceptions;
+
+namespace Inventory.DataAccessLayer
+{
+    public class DistributerDAL
+    {
+        public static List<Distributer> distributerList = new List<Distributer>();
+
+        public bool AddDistributerDAL(Distributer newDistributer)
+        {
+            bool distributerAdded = false;
+            try
+            {
+                distributerList.Add(newDistributer);
+                distributerAdded = true;
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return distributerAdded;
+
+        }
+
+        public List<Distributer> GetAllDistributersDAL()
+        {
+            return distributerList;
+        }
+
+        public Distributer SearchDistributerDAL(string searchDistributerID)
+        {
+            Distributer searchDistributer = null;
+            try
+            {
+                foreach (Distributer item in distributerList)
+                {
+                    if (item.DistributerID == searchDistributerID)
+                    {
+                        searchDistributer = item;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return searchDistributer;
+        }
+
+        public List<Distributer> GetDistributersByNameDAL(string guestName)
+        {
+            List<Distributer> searchDistributer = new List<Distributer>();
+            try
+            {
+                foreach (Distributer item in distributerList)
+                {
+                    if (item.DistributerName == guestName)
+                    {
+                        searchDistributer.Add(item);
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return searchDistributer;
+        }
+
+        public bool UpdateDistributerDAL(Distributer updateDistributer)
+        {
+            bool distributerUpdated = false;
+            try
+            {
+                for (int i = 0; i < distributerList.Count; i++)
+                {
+                    if (distributerList[i].DistributerID == updateDistributer.DistributerID)
+                    {
+                        updateDistributer.DistributerName = distributerList[i].DistributerName;
+                        updateDistributer.DistributerMob= distributerList[i].DistributerMob;
+                        distributerUpdated = true;
+                    }
+                }
+            }
+            catch (SystemException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return   distributerUpdated;
+
+        }
+
+        public bool DeleteDistributerDAL(string deleteDistributerID)
+        {
+            bool distributerDeleted = false;
+            try
+            {
+                Distributer deleteDistributer = null;
+                foreach (Distributer item in distributerList)
+                {
+                    if (item.DistributerID == deleteDistributerID)
+                    {
+                        deleteDistributer  = item;
+                    }
+                }
+
+                if (deleteDistributer!= null)
+                {
+                    distributerList.Remove(deleteDistributer);
+                    distributerDeleted = true;
+                }
+            }
+            catch (DbException ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return    distributerDeleted;
+
+        }
+
+    }
+}
---
+++ Inventory.PresentatioLayer_sowrasree_code/Inventory.Entities/Distributer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Entities
+{
+
+    public class Distributer
+    {
+        private string _distributerID;
+
+        public string DistributerID
+        {
+            get { return _distributerID; }
+            set { _distributerID = value; }
+        }
+        private string _distributerName;
+
+        public string DistributerName
+        {
+            get { return _distributerName; }
+            set { _distributerName = value; }
+        }
+        private string _distributerMob;
+
+        public string DistributerMob
+        {
+            get { return _distributerMob; }
+            set { _distributerMob = value; }
+        }
+        private int _addressID;
+
+        public int AddressID
+        { 
+            get { return _addressID; }
+            set { _addressID = value; }
+        }
+        private string _distributerEmail;
+
+        public string DistributerEmail
+        {
+            get { return _distributerEmail; }
+            set { _distributerEmail = value; }
+        }
+
+
+        public Distributer()
+        {
+            _distributerID = string.Empty;
+            _distributerName = string.Empty;
+            _distributerMob = string.Empty;
+            _addressID = 0;
+            _distributerEmail = string.Empty;
+        }
+    }
+}
