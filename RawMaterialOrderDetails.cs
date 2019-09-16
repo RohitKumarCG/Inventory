a5382d7c029875a42ba06ba5fe510bd5e71d910d
saijahnavi1198 <54538041+saijahnavi1198@users.noreply.github.com>
2019-09-16 12:16:29 +0530

    Add files via upload

---
+++ RawMaterialOrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Entities
+{
+    {
+    public class RawMaterialsOrderDetails
+    {
+        private string _rawMaterialsOrderID;
+        private string _rawMaterialsID;
+        private double _rawMaterialsUnitPrice;
+        private double _rawMaterialsQuantity;
+        private double _rawMaterialsTotalPrice;
+
+        
+        public string RawMaterialsOrderID { get => _rawMaterialsOrderID; set => _rawMaterialsOrderID = value; }
+        public string RawMaterialsID { get => _rawMaterialsID; set => _rawMaterialsID = value; }
+        public double RawMaterialsUnitPrice { get => _rawMaterialsUnitPrice; set => _rawMaterialsUnitPrice = value; }
+        public double RawMaterialsQuantity { get => _rawMaterialsQuantity; set => _rawMaterialsQuantity = value; }
+        public double RawMaterialsTotalPrice { get => _rawMaterialsTotalPrice; set => _rawMaterialsTotalPrice = value; }
+    }
+}
