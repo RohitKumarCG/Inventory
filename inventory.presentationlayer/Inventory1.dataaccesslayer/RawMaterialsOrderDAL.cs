e4a2ec605183fad4c6c598f4ed5e5c270f068f01
ritwik2311 <54567863+ritwik2311@users.noreply.github.com>
2019-09-23 18:44:27 +0530

    Add files via upload

---
+++ inventory.presentationlayer/Inventory1.dataaccesslayer/RawMaterialsOrderDAL.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManagement.Entities;
+using inventory.exceptions;
+
+namespace Inventory.DataAccessLayer
+{
+    public interface IRawMaterialsOrderDAL
+    {
+        double generateTotalPrice(List<int> orders, List<int> quantity);
+        bool AddRawMaterialsOrderDAL(RawMaterialOrder order);
+        bool DeleteRawMaterialsOrderDAL(string RawMaterialsOrderID);
+        bool UpdateRawMaterialsOrderDAL(RawMaterialOrder updateorder);
+        List<RawMaterialOrder> GetAllRawMaterialsOrdersDAL();
+        List<RawMaterialOrder> GetRawMaterialsOrdersByDateDAL(string date);
+        RawMaterialOrder GetRawMaterialsOrdersByOrderIDDAL(string OrderID);
+
+    }
+    public class RawMaterialOrderDAL : IRawMaterialOrderDAL
+    {
+        public static List<RawMaterialOrder> rawmaterialorderList = new List<Order>();// list of product order.
+        int[] price = new int[5] { 10, 20, 30, 40, 50 };
+
+        public double generateTotalPrice(List<int> orders, List<int> quantity)
+        {
+            int j =0;
+            double totalorderprice = 0.00;
+
+            foreach (int i in orders)
+            {
+                totalorderprice += price[i - 1] * quantity[j];
+                j++;
+            }
+
+            return totalorderprice;
+        }
+        public bool AddProductOrderDAL(ProductOrder order)//method for adding product order
+        {
+            bool ProductorderAdded = false;
+            try
+            {
+
+                productorderList.Add(order);
+                ProductorderAdded = true;
+            }
+            catch (Exception ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return ProductorderAdded;
+
+        }
+        public bool  DeleteProductOrderDAL(string productOrderID)//method for deleting product order
+        {
+            bool ProductOrderDeleted = false;
+            try
+            {
+                ProductOrder deleteProductOrder = null;
+                foreach (ProductOrder item in productorderList)
+                {
+                    if (item.ProductOrderID == productOrderID)
+                    {
+                        deleteProductOrder = item;
+                    }
+                }
+
+                if (deleteProductOrder != null)
+                {
+                    productorderList.Remove(deleteProductOrder);
+                    ProductOrderDeleted = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return ProductOrderDeleted;
+
+        }
+        public bool UpdateProductOrderDAL(ProductOrder updateorder)// method for updating product order
+        {
+            bool orderUpdated = false;
+            try
+            {
+                for (int i = 0; i < productorderList.Count; i++)
+                {
+                    if (productorderList[i].ProductOrderID == updateorder.ProductOrderID)
+                    {
+                        productorderList[i] = updateorder;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return orderUpdated;
+
+        }
+        public List<ProductOrder> GetAllproductOrdersDAL() //method to get the list of all product orders
+        {
+            return productorderList;
+        }
+        public List<ProductOrder> GetProductOrdersByDateDAL(string date)// method to get product order by date
+        {
+            List<ProductOrder> searchProductOrder = new List<ProductOrder>();
+            try
+            {
+                foreach (ProductOrder item in productorderList)
+                {
+                    if (item.ProductOrderDate == date)
+                    {
+                        searchProductOrder.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return searchProductOrder;
+        }
+        public ProductOrder GetProductOrdersByOrderIDDAL(string OrderID)// method  to get product orders by ID
+        {
+            List<ProductOrder> searchRawMaterialOrder = new List<ProductOrder>();
+            ProductOrder order = null;
+            try
+            {
+                foreach (ProductOrder item in productorderList)
+                {
+                    if (item.ProductOrderID == OrderID)
+                    {
+                        order = item;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InventoryException(ex.Message);
+            }
+            return order;
+
+        }
+    }
+}
---
+++ inventory.presentationlayer/inventory.presentationlayer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Entities;
+using Inventory.DataAccessLayer;
+using static Inventory.Exceptions.InventoryManagementExceptions;
+using Inventory.BusinessLayer;
+
+namespace Inventory.PresentationLayer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RawMaterialOrderDAL RawMaterialsorderDAL = new RawMaterialsOrderDAL();
+            RawMaterialsOrder RawMaterialsorder = new RawMaterialsOrder();
+            ProductOrderBL RawMaterialsorderBL = new RawMaterialsOrderBL();
+
+            int m = 1;
+            List<int> orders = new List<int>();
+            List<int> quantity = new List<int>();
+            Console.WriteLine("Welcome to DD............");
+            while (m == 1)
+            {
+                Console.WriteLine("enter 1 for fruit basket \n 2 for carbonated water\n 3 for sugar \n 4 for acid \n 5 for sweeteners \n 6 for flavours \n 7 for normal water  ");
+                int n = int.Parse(Console.ReadLine());
+
+                orders.Add(n);
+                Console.WriteLine("Enter the quantity ");
+                int k = int.Parse(Console.ReadLine());
+                quantity.Add(k);
+                Console.WriteLine("Do you want to add more products? Enter 1 for Yes 0 for no");
+                m = int.Parse(Console.ReadLine());
+            }
+            double price =RawMaterialsorderDAL.generateTotalPrice(orders, quantity);
+            Console.WriteLine("Total price of the order" + price);
+            Console.WriteLine("do you want to place the order? \n Enter 1 for yes 0 for no ");
+            m = int.Parse(Console.ReadLine());
+            if (m == 1)
+            {
+                RawMaterialsorder.ProductOrderPrice = price;
+                RawMaterialsorder.DistributorID = "D001";
+                RawMaterialsorderBL.AddProductOrderBL(RawMaterialsorder);
+
+            }
+
+            List<RawMaterialsOrder> list = RawMaterialsorderBL.GetAllProductOrdersBL();
+            foreach (RawMaterialsOrder item in list)
+            {
+                Console.WriteLine("raw material id" + item.ProductOrderID);
+                Console.WriteLine("order price" + item.ProductOrderPrice);
+                Console.WriteLine(" Distributor ID" + item.DistributorID);
+                Console.WriteLine("Order Date" + item.ProductOrderDate);
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
